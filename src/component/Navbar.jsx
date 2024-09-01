2b3e136ac8c56cfa7b5b339de5f048b59965475b
Laiba Khokhar <laibazahid969@gmail.com>
2024-09-01 12:26:02 +0500

    Code Cleaned

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Laiba Zahid Developer</h1>
+      {/* <Navbar/> */}
+    </div>
+  )
+}
+
+export default Navbar
